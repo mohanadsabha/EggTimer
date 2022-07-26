8d179fa8286a1b92919176b75e93c9a6070827c2
Mohanad Abu Sabha <mohannad.naxol@gmail.com>
2022-07-26 03:23:30 +0300

    designed UI

--- EggTimer/AppDelegate.swift
+++ EggTimer/AppDelegate.swift
@@ -2,12 +2,13 @@
 //  AppDelegate.swift
 //  EggTimer
 //
-//  Created by Mohanad on 7/26/22.
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
@@ -15,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
     // MARK: UISceneSession Lifecycle
--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -2,18 +2,20 @@
 //  ViewController.swift
 //  EggTimer
 //
-//  Created by Mohanad on 7/26/22.
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    let softTime = 5
+    let mediumTime = 8
+    let hardTime = 12
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle
     }
-
-
+    
 }
-
