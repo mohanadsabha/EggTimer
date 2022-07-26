3955d4ef5efe1fd15ba80ab222e367efe548e071
Mohanad Abu Sabha <mohannad.naxol@gmail.com>
2022-07-26 05:23:21 +0300

    timer made

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -2,20 +2,33 @@
 //  ViewController.swift
 //  EggTimer
 //
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Mohanad on 26/07/2022.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 8
-    let hardTime = 12
+    @IBOutlet weak var label: UILabel!
+    let eggTime = ["Soft": 3, "Medium": 5, "Hard": 7]
+    var timeRemaining = 60
+    var timer = Timer()
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        timeRemaining = eggTime[hardness]!
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
     }
     
+    @objc func updateTime() {
+        if timeRemaining > 0 {
+            print("\(timeRemaining) seconds.")
+            timeRemaining -= 1
+        }
+        else {
+            timer.invalidate()
+            label.text = "DONE!"
+        }
+    }
 }
